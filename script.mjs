a271d1956e622d1d0f5c61c4085c051c0d0b948d
Jonast96 <Jon.Astveit@hotmail.com>
2022-12-31 14:08:02 +0100

    shows current weather for users location as default

--- script.mjs
+++ script.mjs
@@ -1,16 +1,39 @@
+function showLatitudeAndLongitude() {
+  // Check if the browser supports geolocation
+  if (navigator.geolocation) {
+    // Get the user's current position
+    navigator.geolocation.getCurrentPosition(function (position) {
+      // Get the latitude and longitude from the position object
+      let latitude = position.coords.latitude;
+      let longitude = position.coords.longitude;
+
+      getForecast(latitude, longitude)
+      // Log the latitude and longitude to the console
+      console.log("Latitude: " + latitude);
+      console.log("Longitude: " + longitude);
+    });
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+showLatitudeAndLongitude()
+
+
+
+
 const API_KEY = 'a41bb766615fcb644552efb56837e51d';
 const weatherContainer = document.querySelector(".weather_info")
 
 async function getForecast(lat, lon) {
-    try {
-        const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
-        createWeatherInfo(data)
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+    createWeatherInfo(data)
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 const form = document.querySelector(".form");
@@ -18,56 +41,53 @@ const container = document.querySelector(".search_options");
 const searchInput = document.querySelector(".search");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    getPosition(searchInput.value);
+  e.preventDefault();
+  getPosition(searchInput.value);
 });
 
 async function getPosition(searchInput) {
-    const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=5&appid=${API_KEY}`;
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
-        displaySearchSuggestions(data);
-    } catch (error) {
-        console.error(error);
-    }
+  const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=5&appid=${API_KEY}`;
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+    displaySearchSuggestions(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function displaySearchSuggestions(data) {
-    console.log(data.length);
-    container.innerHTML = "";
-    for (let i = 0; i < data.length; i++) {
-        // Create a button element
-        const button = document.createElement('button');
-        button.classList.add('text-white', 'btn', 'btn-primary', 'd-block', 'my-1');
-        button.innerHTML = `${data[i].name}, ${data[i].country}`;
+  console.log(data.length);
+  container.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    // Create a button element
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-outline-light');
+    button.innerHTML = `${data[i].name}, ${data[i].country}`;
 
-        // Attach an event listener to the button
-        button.addEventListener('click', (event) => {
-            // Your event handler code goes here
-            console.log(data[i].country);
-            getForecast(data[i].lat, data[i].lon);
-            container.innerHTML = ""
+    // Attach an event listener to the button
+    button.addEventListener('click', (event) => {
+      // Your event handler code goes here
+      console.log(data[i].country);
+      getForecast(data[i].lat, data[i].lon);
+      container.innerHTML = ""
 
-        });
+    });
 
-        // Add the button to the container element
-        container.appendChild(button);
-    }
+    // Add the button to the container element
+    container.appendChild(button);
+  }
 }
 
 
 
 function createWeatherInfo(data) {
 
-    let cards = "";
+  let cards = "";
 
-    for (let i = 1; i < 4; i++) {
-        // Convert temperature from Kelvin to Celsius and round to one decimal place
-        let temperature = (data.list[i].main.temp - 273.15).toFixed(1);
-
-        cards += `
+  for (let i = 1; i < 4; i++) {
+    cards += `
         <div class="card" style="width: 20rem">
           <img
             src="https://media4.giphy.com/media/Tdo9vQa9zcfozYJQIY/giphy.gif?cid=790b7611e4cc8b8d487123aed137ae78c9e66fb4c2de41c7&rid=giphy.gif&ct=g"
@@ -78,16 +98,16 @@ function createWeatherInfo(data) {
           <h5 class="card-title">${data.list[i].dt_txt}</h5>
           <ul class="list-group list-group-flush">
             <li class="list-group-item">${data.list[i].weather[0].main}</li>
-            <li class="list-group-item">${temperature}C°</li>
+            <li class="list-group-item">${data.list[i].main.temp.toFixed()}C°</li>
             <li class="list-group-item">${data.list[i].main.humidity}%</li>
           </ul>
         </div>
       `;
-    }
+  }
 
-    weatherContainer.innerHTML = `
+  weatherContainer.innerHTML = `
       <div>
-            <h2 class="text-white">Today's weather, ${data.list[0].dt_txt}</h2>
+            <h2 class="text-white">Today's weather in ${data.city.name}, ${data.list[0].dt_txt}</h2>
             <h4 class="text-white">Current weather</h4>
           </div>
           <div>
@@ -96,7 +116,7 @@ function createWeatherInfo(data) {
               ${data.list[0].weather[0].main}
               </li>
               <li class="bg-light list-group-item rounded mx-3 px-3 py-1 h3">
-              ${(data.list[0].main.temp - 273.15).toFixed(1)}C°
+              ${data.list[0].main.temp.toFixed(0)}C°
               </li>
               <li class="bg-light list-group-item rounded mx-3 px-3 py-1 h3">
               ${data.list[0].main.humidity}%
