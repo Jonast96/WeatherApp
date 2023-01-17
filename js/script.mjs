67be33a950074efbd06e5d2c200da693de20b376
Jonast96 <Jon.Astveit@hotmail.com>
2023-01-17 14:52:12 +0100

    added list of next 3 hours

--- js/script.mjs
+++ js/script.mjs
@@ -30,7 +30,6 @@ async function getForecast(lat, lon) {
 
 async function createHtml(data) {
   const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${data.geometry.coordinates[1]}&lon=${data.geometry.coordinates[0]}`;
-  const container = document.querySelector(".weather_info")
   let oldId = getOldId(data.properties.timeseries[0].data.next_1_hours.summary.symbol_code);
   const response = await fetch(url);
   const locationData = await response.json();
@@ -42,8 +41,16 @@ async function createHtml(data) {
     wind: data.properties.timeseries[0].data.instant.details.wind_speed
   }
 
-  console.log(weatherData)
+  currentReportHtml(weatherData, oldId, location)
+  nextHourReportHtml(data, oldId)
 
+
+
+
+}
+
+function currentReportHtml(weatherData, img, location) {
+  const container = document.querySelector(".container_section1")
   container.innerHTML = `
   <p class="display-6 text-light p-5">${location}</p>
   <div class="text-white row align-items-center border border-opacity-25 rounded d-flex justify-content-center">
@@ -51,9 +58,41 @@ async function createHtml(data) {
     <p class="fs-3 order-lg-1 order-2 col-4 col-lg-2">${weatherData.weather}</p>
     <p class="fs-3 order-lg-1 order-2 col-4 col-lg-2">${weatherData.amount} mm</p>
     <p class="fs-3 order-lg-1 order-2 col-4 col-lg-2">${weatherData.wind} m/s</p>
-    <img style="width:150px" class="img-fluid col-12 col-lg-2 order-1 order-lg-2" src="/images/${oldId.toString().padStart(2, "0")}.png" alt="" />
+    <img style="width:150px" class="img-fluid col-12 col-lg-2 order-1 order-lg-2" src="/images/${img.toString().padStart(2, "0")}.png" alt="" />
   </div>
     `
-
 }
 
+function nextHourReportHtml(data, img) {
+
+  const container = document.querySelector(".next_hour")
+
+
+
+  for (let i = 0; i < 3; i++) {
+
+    const date = new Date(data.properties.timeseries[i].time);
+    const options = { hour: 'numeric', minute: 'numeric' };
+    const time = date.toLocaleTimeString([], options)
+    console.log(date.toLocaleTimeString([], options));
+
+    container.innerHTML += `
+    <div
+    class="text-light py-1 border-bottom d-flex justify-content-around align-items-center"
+  >
+    <p class="fs-4">${time}</p>
+    <img style="width:80px" class="img-fluid col-12 col-lg-2 order-1 order-lg-2" src="/images/${img.toString().padStart(2, "0")}.png" alt="" />
+    <p class="fs-5">${data.properties.timeseries[i].data.instant.details.air_temperature}</p>
+    <p class="fs-5">${data.properties.timeseries[i].data.next_1_hours.details.precipitation_amount} mm</p>
+    <p class="fs-5">${data.properties.timeseries[i].data.instant.details.wind_speed} m/s</p>
+  </div>
+  
+  `
+
+
+  }
+
+
+
+
+} 
