2c0d31b055553424fff5a61905ad2f222f6c033c
Jonast96 <Jon.Astveit@hotmail.com>
2023-01-19 00:56:39 +0100

    added search functionality and removed view more for now

--- js/createHtml.js
+++ js/createHtml.js
@@ -1,46 +1,23 @@
 export function nextHourReportHtml(data) {
-  let loopLimit = 3;
   const container = document.querySelector(".next_hour")
-  for (let i = 0; i < loopLimit; i++) {
+
+  for (let i = 0; i < 3; i++) {
     const date = new Date(data.properties.timeseries[i].time);
     const options = { hour: 'numeric' };
-    const time = date.toLocaleTimeString([], options)
-
+    const time = date.toLocaleTimeString([], options);
     container.innerHTML += `
-      <div
-      class="text-light py-1 border-bottom d-flex justify-content-around align-items-center"
-    >
-      <p class="fs-4">${time}</p>
-      <img style="width:80px" class="img-fluid col-12 col-lg-2 order-1 order-lg-2" src="/images/${data.properties.timeseries[i].data.next_1_hours.summary.symbol_code}.png" alt="" />
-      <p class="fs-5">${Math.floor(data.properties.timeseries[i].data.instant.details.air_temperature)}°c</p>
-      <p class="fs-5">${Math.floor(data.properties.timeseries[i].data.next_1_hours.details.precipitation_amount)} mm</p>
-      <p class="fs-5">${Math.floor(data.properties.timeseries[i].data.instant.details.wind_speed)} m/s</p>
+      <div class="text-light py-1 border-bottom d-flex justify-content-around align-items-center">
+        <p class="fs-4">${time}</p>
+        <img style="width:80px" class="img-fluid col-12 col-lg-2 order-1 order-lg-2" src="/images/${data.properties.timeseries[i].data.next_1_hours.summary.symbol_code}.png" alt="" />
+        <p class="fs-5">${Math.floor(data.properties.timeseries[i].data.instant.details.air_temperature)}°c</p>
+        <p class="fs-5">${Math.floor(data.properties.timeseries[i].data.next_1_hours.details.precipitation_amount)} mm</p>
+        <p class="fs-5">${Math.floor(data.properties.timeseries[i].data.instant.details.wind_speed)} m/s</p>
       </div>
     `
   }
-
-  const viewMoreButton = document.querySelector("#view-more-button");
-  viewMoreButton.addEventListener("click", () => {
-    for (let i = loopLimit; i < loopLimit + 5; i++) {
-      const date = new Date(data.properties.timeseries[i].time);
-      const options = { hour: 'numeric' };
-      const time = date.toLocaleTimeString([], options)
-
-      container.innerHTML += `
-      <div
-      class="text-light py-1 border-bottom d-flex justify-content-around align-items-center"
-    >
-      <p class="fs-4">${time}</p>
-      <img style="width:80px" class="img-fluid col-12 col-lg-2 order-1 order-lg-2" src="/images/${data.properties.timeseries[i].data.next_1_hours.summary.symbol_code}.png" alt="" />
-      <p class="fs-5">${Math.floor(data.properties.timeseries[i].data.instant.details.air_temperature)}°c</p>
-      <p class="fs-5">${Math.floor(data.properties.timeseries[i].data.next_1_hours.details.precipitation_amount)} mm</p>
-      <p class="fs-5">${Math.floor(data.properties.timeseries[i].data.instant.details.wind_speed)} m/s</p>
-      </div>
-    `
-    }
-    loopLimit += 5;
-  });
 }
+
+
 export function currentReportHtml(weatherData, img, location) {
   const container = document.querySelector(".container_section1")
   container.innerHTML = `
--- js/script.mjs
+++ js/script.mjs
@@ -3,6 +3,7 @@ import {
   nextHourReportHtml,
   currentReportHtml
 } from "./createHtml.js";
+const container = document.querySelector(".next_hour")
 function showLatitudeAndLongitude() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -16,39 +17,48 @@ function showLatitudeAndLongitude() {
 }
 showLatitudeAndLongitude()
 
+async function getLocationFromLatLon(lat, lon) {
+  const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&zoom=18&addressdetails=1`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 
 async function getForecast(lat, lon) {
   try {
-    const blurOnLoad = document.querySelector(".blur")
     const apiUrl = `https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=${lat}&lon=${lon}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
     console.log(data);
-    createHtml(data)
-    blurOnLoad.classList.remove("blur")
+    const weatherData = {
+      temp: data.properties.timeseries[0].data.instant.details.air_temperature,
+      weather: data.properties.timeseries[0].data.next_1_hours.summary.symbol_code,
+      amount: data.properties.timeseries[0].data.next_1_hours.details.precipitation_amount,
+      wind: data.properties.timeseries[0].data.instant.details.wind_speed,
+      lat: data.geometry.coordinates[1],
+      lon: data.geometry.coordinates[0]
+    }
+
+    const test = await getLocationFromLatLon(weatherData.lat, weatherData.lon)
+
+    let weatherId = data.properties.timeseries[0].data.next_1_hours.summary.symbol_code
+    currentReportHtml(weatherData, weatherId, test.address.county)
+    nextHourReportHtml(data)
+
   } catch (error) {
     console.error(error);
   }
 }
 
-async function createHtml(data) {
-  const url = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${data.geometry.coordinates[1]}&lon=${data.geometry.coordinates[0]}`;
-  let weatherId = data.properties.timeseries[0].data.next_1_hours.summary.symbol_code
-
-  const response = await fetch(url);
-  const locationData = await response.json();
-  const location = `${locationData.address.city}, ${locationData.address.country}`
-  const weatherData = {
-    temp: data.properties.timeseries[0].data.instant.details.air_temperature,
-    weather: data.properties.timeseries[0].data.next_1_hours.summary.symbol_code,
-    amount: data.properties.timeseries[0].data.next_1_hours.details.precipitation_amount,
-    wind: data.properties.timeseries[0].data.instant.details.wind_speed
-  }
-
-  currentReportHtml(weatherData, weatherId, location)
-  nextHourReportHtml(data)
-}
-
+const resultsList = document.querySelector(".results");
+const searchInput = document.querySelector(".search");
+const form = document.querySelector(".form_container");
 
 
 async function getLatLng(locationName) {
@@ -56,16 +66,41 @@ async function getLatLng(locationName) {
     const url = `https://nominatim.openstreetmap.org/search?q=${locationName}&format=json`;
     const response = await fetch(url);
     const data = await response.json();
-
-    if (data.length > 0) {
-      const lat = data[0].lat;
-      const lng = data[0].lon;
-      console.log(data)
-      return { lat, lng };
-    } else {
-      throw new Error("No results found for the given location name.");
+    console.log(data)
+    let list = "";
+    for (let i = 0; i < 5; i++) {
+      try {
+        let lat = data[i].lat;
+        let lon = data[i].lon;
+        list += `<li class=" list-group-item bg-transparent border-white w-75 mx-auto" data-lat="${lat}" data-lon="${lon}"><a class="text-white" href="#">${data[i].display_name}</a></li>`;
+      } catch (error) {
+        console.error(`An error occurred: ${error}`);
+      }
     }
+    resultsList.innerHTML = list;
+
   } catch (error) {
-    console.log("An error occurred: " + error);
+    console.error(`An error occurred: ${error}`);
   }
 }
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  getLatLng(searchInput.value);
+});
+
+
+
+
+resultsList.addEventListener("click", e => {
+  if (e.target.tagName === "A") {
+    container.innerHTML = ""
+    let lat = e.target.parentNode.getAttribute("data-lat");
+    let lon = e.target.parentNode.getAttribute("data-lon");
+    getForecast(lat, lon)
+
+    resultsList.innerHTML = ""
+    searchInput.value = ""
+
+  }
+});
